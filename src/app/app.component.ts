9f8bcb354a4d5508187fa45d037be95f246a2115
pmachaux <pierre.machaux@gmail.com>
2017-01-09 20:07:39 -0500

    First commit: Init snake with movement to the left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {SnakeService} from "./snake-service.service";
+import {Snake} from "./snake";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [SnakeService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Snake-Angular2';
+  score = 0;
+  direction = 'left';
+  snake: Snake;
+  // get the element with the #chessCanvas on it
+  @ViewChild("snakeCanvas") snakeCanvas: ElementRef;
+
+  constructor(private snakeService: SnakeService){
+    this.snake = new Snake();
+  }
+
+  ngAfterViewInit() { // wait for the view to init before using the element
+    this.snakeService.drawSnake(this.snakeCanvas.nativeElement.getContext("2d"), this.snake);
+   setInterval(() => {
+     this.snakeService.clearCanvas(this.snakeCanvas.nativeElement.getContext("2d"));
+      this.snake = this.snakeService.moveSnake(this.snake, this.direction);
+      this.snakeService.drawSnake(this.snakeCanvas.nativeElement.getContext("2d"), this.snake);
+    }, 1000);
+  }
 }
